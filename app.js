49ddc9b2da2640edf2b386dbdc667deebcafc0bd
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2016-11-12 15:13:53 -0500

    -> production

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger('production'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
